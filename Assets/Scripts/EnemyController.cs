be498ca3c3bd488b3bed51d6b20459a359495727
本多優仁 <masato0116honda@gmail.com>
2026-06-16 00:14:35 +0900

    feat：敵の剣攻撃モーションを仮実装

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,8 +8,23 @@ public class EnemyController : MonoBehaviour
     private float attackRange = 2f;
     private float attackCooldown = 1f;
     private float attackTimer = 0f;
-    private bool isAttacking = false;
+
+    private enum EnemyState
+    {
+        Chase,
+        Windup,
+        Attack,
+        Cooldown,
+    }
+
+    private EnemyState state = EnemyState.Chase;
+    private float windupDuration = 0.5f;
+    private float windupTimer = 0f;
+    private float attackDuration = 0.3f;
+    private float attackStateTimer;
+
     [SerializeField] private GameObject target;
+    [SerializeField] private Transform weapon;
 
 
     void Start()
@@ -21,7 +36,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-     void Update()
+    void Update()
     {
         if (target == null) return;
 
@@ -42,10 +57,64 @@ public class EnemyController : MonoBehaviour
         attackTimer -= Time.deltaTime;
 
 
-        if (distance <= attackRange && attackTimer <= 0f)
+        if (distance <= attackRange && attackTimer <= 0f && state == EnemyState.Chase)
         {
-            playerStatus.TakeDamage(10);
-            attackTimer = attackCooldown;
+            state = EnemyState.Windup;
+            windupTimer = windupDuration;
+        }
+
+        if (state == EnemyState.Windup)
+        {
+            windupTimer -= Time.deltaTime;
+
+            agent.ResetPath();
+
+            weapon.localRotation = Quaternion.RotateTowards
+            (
+                weapon.localRotation,
+                Quaternion.Euler(-90f, 0f, 0f),
+                180f * Time.deltaTime
+            );
+
+            if (windupTimer <= 0f)
+            {
+                state = EnemyState.Attack;
+                attackStateTimer = attackDuration;
+            }
+
+            return;
+        }
+
+        if (state == EnemyState.Attack)
+        {
+            attackStateTimer -= Time.deltaTime;
+
+            weapon.localRotation = Quaternion.RotateTowards
+            (
+                weapon.localRotation,
+                Quaternion.Euler(90f, 0f, 0f),
+                720f * Time.deltaTime
+            );
+
+            if (attackStateTimer <= 0f)
+            {
+                playerStatus.TakeDamage(10);
+                attackTimer = attackCooldown;
+                state = EnemyState.Cooldown;
+            }
+
+            return;
+        }
+
+        if (state == EnemyState.Cooldown)
+        {
+            if (attackTimer <= 0f)
+            {
+                weapon.localRotation = Quaternion.identity;
+                state = EnemyState.Chase;
+            }
+
+            return;
         }
     }
 }
